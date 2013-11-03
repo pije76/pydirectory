0102973b680313a2673349b878109ce3ad617f93
Panjianom Adipratomo <panjul76@hotmail.com>
2013-11-03 21:26:49 +0700

    commit

---
+++ pydirectory/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls import patterns, include, url
+from django.conf import settings
+from django.utils.functional import curry
+from django.views.defaults import *
+
+# Uncomment the next two lines to enable the admin:
+from django.contrib import admin
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'djangoproject.views.home', name='home'),
+    # url(r'^djangoproject/', include('djangoproject.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    url(r'^admin/', include(admin.site.urls)),
+)
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+)
+
+handler500 = curry(server_error, template_name='500.html')
+handler404 = curry(page_not_found, template_name='404.html')
+handler403 = curry(permission_denied, template_name='403.html')
